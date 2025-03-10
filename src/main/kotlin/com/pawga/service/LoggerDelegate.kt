e0924d7d88967a30082cefb269fa66367e831b83
sivannikov <pawga777@gmail.com>
2025-03-10 12:20:16 +0300

    update bpm

---
+++ src/main/kotlin/com/pawga/service/LoggerDelegate.kt
@@ -0,0 +1,37 @@
+package com.pawga.service
+
+import jakarta.inject.Singleton
+import org.camunda.bpm.engine.delegate.DelegateExecution
+import org.camunda.bpm.engine.delegate.JavaDelegate
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+
+/**
+ * Created by sivannikov on 27.02.2025 17:42
+ */
+
+@Singleton
+class LoggerDelegate : JavaDelegate {
+    private val log: Logger = LoggerFactory.getLogger(this::class.java)
+
+    override fun execute(execution: DelegateExecution) {
+        log.info(
+            "\n\n  ... LoggerDelegate invoked by "
+                    + "activtyName='" + execution.getCurrentActivityName() + "'"
+                    + ", activtyId=" + execution.getCurrentActivityId()
+                    + ", processDefinitionId=" + execution.getProcessDefinitionId()
+                    + ", processInstanceId=" + execution.getProcessInstanceId()
+                    + ", businessKey=" + execution.getProcessBusinessKey()
+                    + ", executionId=" + execution.getId()
+                    + ", variables=" + execution.getVariables()
+                    + " \n\n"
+        )
+        val decisionResult = execution.getVariable("decision") as List<*>
+        log.info("decisionResult.count ={}", decisionResult.size)
+
+        for (item in decisionResult) {
+            println("Item: $item")
+        }
+    }
+}
